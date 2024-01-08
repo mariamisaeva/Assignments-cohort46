01c9311faa3d9303f39df31d414adb311a1f9e3c
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-01-08 15:32:29 +0100

    APIs week1 DONE

--- 3-UsingAPIs/Week1/assignment/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/assignment/ex1-johnWho.js
@@ -8,26 +8,29 @@ Rewrite this function, but replace the callback syntax with the Promise syntax:
 - If the Promise `rejects`, pass an error as the argument to reject with: "You 
   didn't pass in a first name!"
 ------------------------------------------------------------------------------*/
-// TODO see above
-const getAnonName = (firstName, callback) => {
-  setTimeout(() => {
-    if (!firstName) {
-      callback(new Error("You didn't pass in a first name!"));
-      return;
-    }
 
-    const fullName = `${firstName} Doe`;
+const getAnonName = (firstName) => {
 
-    callback(fullName);
-  }, 1000);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!firstName) {
+                reject(new Error("You didn't pass in a first name!"));
+                return;
+            }
+
+            const fullName = `${firstName} Doe`;
+
+            resolve(fullName);
+        }, 1000);
+    });
 };
 
 function main() {
-  getAnonName('John', console.log);
+    getAnonName('John', console.log);
 }
 
 // ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
-  main();
+    main();
 }
 module.exports = getAnonName;
